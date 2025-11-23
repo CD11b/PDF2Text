1683ca607b076b5097a4359c88a38b87932fff97
CD11b <38716745+CD11b@users.noreply.github.com>
2025-11-23 14:41:09 -0500

    take linecontext out of main

---
+++ models/decisions/line_context.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from .context_types import *
+
+@dataclass
+class LineContext:
+    line_group: list
+    position_in_paragraph: PositionInParagraph
+    indentation: LineIndentation
+    region: VerticalRegion
+    margin_position: MarginPosition
+    density: LineDensity
+    font_name: LineFontName
+    font_size: LineFontSize
+
+    @classmethod
+    def create(cls, layout, line_group, groups_iter, result):
+        return cls(
+            line_group=line_group,
+            position_in_paragraph=layout.get_position_in_paragraph(line_group, groups_iter, result),
+            indentation=layout.get_line_indentation(line_group, groups_iter, result),
+            region=layout.get_line_region(line_group),
+            margin_position=layout.get_line_position(line_group),
+            density=layout.get_line_density(line_group),
+            font_name=layout.get_font_name(line_group),
+            font_size=layout.get_font_size(line_group)
+        )
