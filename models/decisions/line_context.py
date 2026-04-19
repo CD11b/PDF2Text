30c1a64e06e4e744974a1bc0391f7be9f7b61104
CD11b <38716745+CD11b@users.noreply.github.com>
2026-04-19 17:00:53 -0400

    bug fix

--- models/decisions/line_context.py
+++ models/decisions/line_context.py
@@ -21,12 +21,13 @@ class LineContext:
         line_start_y = line_group[0].start_y
         next_group = groups_iter.peek()
         previous_start_x = result[-1].start_x if len(result) > 1 else None
+        previous_start_y = result[-1].start_y if len(result) > 1 else None
         next_start_x = next_group[0].start_x if next_group else None
         next_start_y = next_group[0].start_y if next_group else None
 
         return cls(
             line_group=line_group,
-            position_in_paragraph=layout.paragraph_type.classify_position(line_start_y, previous_start_x, next_start_y),
+            position_in_paragraph=layout.paragraph_type.classify_position(line_start_y, previous_start_y, next_start_y),
             indentation=layout.paragraph_type.classify_indentation(line_start_x, previous_start_x, next_start_x),
             region=layout.line_region.classify_vertical_region(line_group),
             margin_position=layout.line_position.classify_left_margin(line_group),
