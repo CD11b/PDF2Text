9b82de79ec3d086b524c44162079c02c642a5d6c
CD11b <38716745+CD11b@users.noreply.github.com>
2026-04-19 12:24:42 -0400

    make action meta information part of the action class

--- models/decisions/decision.py
+++ models/decisions/decision.py
@@ -1,11 +1,30 @@
 from dataclasses import dataclass
-from enum import Enum, auto
+from enum import Enum
+import logging
 
 class Action(Enum):
     """Actions that can be taken on a line group."""
-    COLLECT = auto()
-    SKIP = auto()
-    UNHANDLED = auto()
+
+    COLLECT = (True, logging.DEBUG, "Collected")
+    SKIP = (False, logging.INFO, "Skipped")
+    UNHANDLED = (False, logging.WARNING, "Unhandled")
+
+    def __init__(self, should_collect: bool, log_level: int, label: str):
+        self._should_collect = should_collect
+        self._log_level = log_level
+        self._label = label
+
+    @property
+    def should_collect(self) -> bool:
+        return self._should_collect
+
+    @property
+    def log_level(self) -> int:
+        return self._log_level
+
+    @property
+    def action_label(self) -> str:
+        return self._label
 
 @dataclass(frozen=True, slots=True)
 class Decision:
