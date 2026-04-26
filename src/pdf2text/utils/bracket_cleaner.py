692d5e5ba78c4895c247ea87bf46bb4b5740b0e2
CD11b <38716745+CD11b@users.noreply.github.com>
2026-04-26 16:14:02 -0400

    bugfix

--- src/pdf2text/utils/bracket_cleaner.py
+++ src/pdf2text/utils/bracket_cleaner.py
@@ -44,7 +44,7 @@ class BracketCleaner:
         cleaned_text = after_close.lstrip()
         self.hanging_open = None
 
-        logging.debug(f"Resolved hanging bracket: text={cleaned_text}")
+        logger.debug(f"Resolved hanging bracket: text={cleaned_text}")
         return cleaned_text
 
     def handle_hanging_close(self, before_open, after_close):
@@ -58,13 +58,13 @@ class BracketCleaner:
     def handle_opening_bracket(self, text, lines_iter):
 
         if self.current_close in text:
-            logging.debug(f"Found open and close brackets in line: {text}")
+            logger.debug(f"Found open and close brackets in line: {text}")
             cleaned_text = self.clean_and_join(text)
 
         elif lines_iter.peek() and self.current_close in lines_iter.peek().text:
             next_line = next(lines_iter)
             combined = text + " " + next_line.text
-            logging.debug(f"Found open and close brackets across consecutive lines: {text}, {next_line}")
+            logger.debug(f"Found open and close brackets across consecutive lines: {text}, {next_line}")
             cleaned_text = self.clean_and_join(combined)
 
         else:
@@ -83,12 +83,12 @@ class BracketCleaner:
                 break
 
         if found_close:
-            logging.debug(f"Found open and close brackets across multiple lines: {text} ... {buffer_lines[-1]}")
+            logger.debug(f"Found open and close brackets across multiple lines: {text} ... {buffer_lines[-1]}")
             block_text = "\n".join(buffer_lines)
             cleaned_text = self.clean_and_join(block_text)
             self.hanging_open = None
         else:
-            logging.debug(f"Found hanging open bracket: {text}")
+            logger.debug(f"Found hanging open bracket: {text}")
             self.hanging_open = self.current_open
             cleaned_text = text.partition(self.current_open)[0].rstrip()
 
@@ -99,11 +99,11 @@ class BracketCleaner:
         before_open, after_close = self.partition_by_brackets(text)
 
         if self.current_close in before_open:  # Author typo: hanging close
-            logging.warning(f"Cleaning [CASE: Author typo - Hanging Close]: line={text}")
+            logger.warning(f"Cleaning [CASE: Author typo - Hanging Close]: line={text}")
             cleaned_text = self.handle_hanging_close(before_open, after_close)
 
         else:
-            logging.debug(f"Cleaning [CASE: Brackets Closed on Same Line]: line={text}")
+            logger.debug(f"Cleaning [CASE: Brackets Closed on Same Line]: line={text}")
             cleaned_text = ''.join([before_open.rstrip(), after_close])
 
         return cleaned_text
@@ -120,7 +120,7 @@ class BracketCleaner:
                 line_cleaned = False
                 while not line_cleaned:
                     if self.hanging_open and close_b in text:
-                        logging.debug(f"Found closing bracket of hanging open: {line}")
+                        logger.debug(f"Found closing bracket of hanging open: {line}")
                         cleaned_text = self.handle_hanging_bracket(text)
 
                     elif open_b in text:
