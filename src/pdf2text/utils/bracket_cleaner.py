67dcbbcccff36406955664871d3d4c9218cb1aa0
CD11b <38716745+CD11b@users.noreply.github.com>
2026-04-28 13:37:21 -0400

    avoid scanning for all pairs for each span; avoid shadowing open/close

--- src/pdf2text/utils/bracket_cleaner.py
+++ src/pdf2text/utils/bracket_cleaner.py
@@ -7,8 +7,8 @@ from dataclasses import dataclass
 @dataclass
 class BracketCleanerContext:
     multipage_open: str | None = None
-    open: str | None = None
-    close: str | None = None
+    open_b: str | None = None
+    close_b: str | None = None
 
 logger = logging.getLogger(__name__)
 
@@ -17,21 +17,19 @@ class BracketCleaner:
     def __init__(self, context: BracketCleanerContext):
         self.context = context
 
-    def prioritized_pairs(self):
-
-        pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}
+    def prioritized_pairs(self, pairs):
 
         if self.context.multipage_open:
             yield self.context.multipage_open, pairs[self.context.multipage_open]
 
-        for open, close in pairs.items():
-            if open != self.context.multipage_open:
-                yield open, close
+        for open_b, close_b in pairs.items():
+            if open_b != self.context.multipage_open:
+                yield open_b, close_b
 
     def partition_by_brackets(self, text):
 
-        before_open, _, _ = text.partition(self.context.open)
-        _, _, after_close = text.partition(self.context.close)
+        before_open, _, _ = text.partition(self.context.open_b)
+        _, _, after_close = text.partition(self.context.close_b)
 
         return before_open, after_close
 
@@ -39,10 +37,10 @@ class BracketCleaner:
 
         before_open, after_close = self.partition_by_brackets(text)
 
-        if self.context.close in before_open:  # Author typo: hanging close
-            before_typo, _, after_typo = before_open.partition(self.context.close)
+        if self.context.close_b in before_open:  # Author typo: hanging close
+            before_typo, _, after_typo = before_open.partition(self.context.close_b)
             before_open = before_typo + after_typo
-            _, _, after_close = after_close.partition(self.context.close)
+            _, _, after_close = after_close.partition(self.context.close_b)
 
         return before_open.rstrip() + after_close
 
@@ -60,7 +58,7 @@ class BracketCleaner:
         for span in spans[1:]:
             buffer_lines.append(span.text)
             consumed += 1
-            if self.context.close in span.text:
+            if self.context.close_b in span.text:
                 found_close = True
                 break
 
@@ -68,17 +66,31 @@ class BracketCleaner:
             logger.debug(f"Found open and close brackets across multiple lines: {text} ... {buffer_lines[-1]}")
             block_text = "\n".join(buffer_lines)
             cleaned_text = self.clean_and_join(block_text)
-            if self.context.multipage_open and self.context.open == self.context.multipage_open:
+            if self.context.multipage_open and self.context.open_b == self.context.multipage_open:
                 self.context.multipage_open = None
         else:
             logger.debug(f"Found hanging open bracket: {text}")
-            self.context.multipage_open = self.context.open
-            cleaned_text = text.partition(self.context.open)[0].rstrip()
+            self.context.multipage_open = self.context.open_b
+            cleaned_text = text.partition(self.context.open_b)[0].rstrip()
 
         return cleaned_text, consumed
 
+    @staticmethod
+    def identify_pairs(spans):
+        pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}
+        found = {}
+
+        chars = set(''.join(span.text.lower() for span in spans))
+
+        for open_b, close_b in pairs.items():
+            if open_b in chars or close_b in chars:
+                found[open_b] = close_b
+
+        return found
 
     def clean_brackets(self, spans: list[Span]) -> list[Span]:
+
+        pairs = self.identify_pairs(spans)
 
         result = []
         i = 0
@@ -86,15 +98,15 @@ class BracketCleaner:
 
         while i < len(spans):
             text_buffer = spans[i].text
-            for self.context.open, self.context.close in self.prioritized_pairs():
+            for self.context.open_b, self.context.close_b in self.prioritized_pairs(pairs):
                 while True:
-                    if self.context.multipage_open and self.context.close in text_buffer:
+                    if self.context.multipage_open and self.context.close_b in text_buffer:
                         text_buffer = self.close_multipage_bracket(text_buffer)
 
-                    elif self.context.open in text_buffer and self.context.close in text_buffer:
+                    elif self.context.open_b in text_buffer and self.context.close_b in text_buffer:
                         text_buffer = self.clean_and_join(text_buffer)
 
-                    elif self.context.open in text_buffer:
+                    elif self.context.open_b in text_buffer:
                         text_buffer, consumed = self.handle_multiline_bracket(text_buffer, spans[i:])
                         j += consumed
 
@@ -108,5 +120,6 @@ class BracketCleaner:
 
             i += 1 + j
             j = 0
+            self.context.open_b = self.context.close_b = None
 
         return result
